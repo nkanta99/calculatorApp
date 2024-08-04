374643a60ab61bbeaa7554d32ae41f4f9612a983
isaac nkanta <170721134+nkanta99@users.noreply.github.com>
2024-08-04 21:30:37 +0100

    App numerics

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
+
+const App = () => {
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState("");
+    const buttons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", ".", "=", "C"];
+    
+}
